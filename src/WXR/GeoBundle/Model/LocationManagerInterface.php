01ac705855a0042c4b3f4b8c56646a47703093ce
Lionel Gaillard <lionel@wxrstudios.com>
2012-10-03 14:57:14 +0200

    Fixed Entity\LocationManagerInterface

--- src/WXR/GeoBundle/Model/LocationManagerInterface.php
+++ src/WXR/GeoBundle/Model/LocationManagerInterface.php
@@ -4,6 +4,6 @@ namespace WXR\GeoBundle\Model;
 
 use WXR\CommonBundle\Model\BaseManagerInterface;
 
-interface LocationManagerInterface extends WXR\CommonBundle\Model\BaseManagerInterface
+interface LocationManagerInterface extends BaseManagerInterface
 {
 }

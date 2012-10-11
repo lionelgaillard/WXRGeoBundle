d3e4403c14291db08216db5dc0874023b4e0c546
Lionel Gaillard <lionel@wxrstudios.com>
2012-10-11 12:59:05 +0200

    Removed unique constraint

--- src/WXR/GeoBundle/Admin/Entity/CountryAdmin.php
+++ src/WXR/GeoBundle/Admin/Entity/CountryAdmin.php
@@ -2,22 +2,8 @@
 
 namespace WXR\GeoBundle\Admin\Entity;
 
-use Sonata\AdminBundle\Validator\ErrorElement;
-
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
 use WXR\GeoBundle\Admin\Model\CountryAdmin as BaseCountryAdmin;
 
 class CountryAdmin extends BaseCountryAdmin
 {
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        parent::validate($errorElement, $object);
-
-        $errorElement
-            ->with('iso')
-                ->validate()
-            ->end()
-        ;
-    }
 }
